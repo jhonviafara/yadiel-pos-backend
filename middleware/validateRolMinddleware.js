11754b175fa2325924ceb5b3e35dd65b3be65bc3
jhon viafara <jhon1144105@gmail.com>
2024-12-29 17:10:43 -0300

    actualizacion

--- middleware/validateRolMinddleware.js
+++ middleware/validateRolMinddleware.js
@@ -1,18 +1,22 @@
-import { log } from "console";
 import dotenv, { config } from "dotenv";
 import jwt from "jsonwebtoken";
 
 dotenv.config()
 
 
-export const validateRol = (req,res,next)=>{
-const token = req.headers["authorization"] //obtengo el token de el post que hace el cliente
-    if (!token) {
+export const validateRol =(rolValido) => (req,res,next)=>{
+const token = req.headers["authorization"];//obtengo el token de el post que hace el cliente
+if (!token) {
         return res.status(403).json({ message: "Token requerido" });
       }
+    
+    
       try {
-        const decoded = jwt.verify(token.split(" "[1]),process.env.SECRET_KEY)
-      if (decoded.rol !== 6323) {
+        const decoded = jwt.verify(token.split(" ")[1],process.env.SECRET_KEY)
+      decoded.rol = parseInt(decoded.rol,10)
+        console.log(decoded.rol);
+        
+      if (decoded.rol !== rolValido) {
             return  res.status(403).json({message:"rol no  autorizado para acceder"})
         } 
         req.user = decoded;
